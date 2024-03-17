a8c9c0d23c3c7564e3afedc5b74d852fbd555b4d
Mateo Ap <mateo.aponte@masivian.com>
2024-03-16 23:44:53 -0500

    feat: Add chain of behaviour

--- typescript/behaviour/chainOfResponsability.ts
+++ typescript/behaviour/chainOfResponsability.ts
@@ -74,3 +74,90 @@ const initApp = () => {
 };
 
 initApp();
+
+// Clase que muestra el mensaje
+class ClientAtention {
+  public requestType: RequestType;
+  public message: String;
+  constructor(requestType: RequestType, message: String) {
+    this.requestType = requestType;
+    this.message = message;
+  }
+  public setMessage(message: String) {
+    this.message = message;
+  }
+}
+// Diccionario de Tipos
+enum RequestType {
+  Complaint,
+  Answer,
+  TechnicalAssistance,
+}
+// Clase contrato para heredar y hacer polimorfismo
+abstract class Requests {
+  private _nextRequest: Requests;
+
+  // Método de punteros
+  public setNextRequest(nextRequest: Requests) {
+    if (this._nextRequest !== null) {
+      this._nextRequest = nextRequest;
+    }
+  }
+  // Método que pasa al siguiente manejador la petición
+  public requestHandler(clientRequest: ClientAtention) {
+    if (this._nextRequest !== null) {
+      return this._nextRequest.requestHandler(clientRequest);
+    }
+    return 0;
+  }
+}
+// Clase heredada que hace polimorfismo
+class ComplaintRequest extends Requests {
+  public requestHandler(clientRequest: ClientAtention) {
+    if (clientRequest.requestType === RequestType.Complaint) {
+      clientRequest.setMessage('Solicitud es una Queja: ' + clientRequest.message);
+      return clientRequest;
+    }
+    return super.requestHandler(clientRequest);
+  }
+}
+// Clase heredada que hace polimorfismo
+class AnswerRequest extends Requests {
+  public requestHandler(clientRequest: ClientAtention) {
+    if (clientRequest.requestType === RequestType.Answer) {
+      clientRequest.setMessage('Solicitud es una Pregunta: ' + clientRequest.message);
+      return clientRequest;
+    }
+    return super.requestHandler(clientRequest);
+  }
+}
+// Clase heredada que hace polimorfismo
+class TechnicalAssistanceRequest extends Requests {
+  public requestHandler(clientRequest: ClientAtention) {
+    if (clientRequest.requestType === RequestType.TechnicalAssistance) {
+      clientRequest.setMessage('Solicitud es una Pregunta: ' + clientRequest.message);
+      return clientRequest;
+    }
+    return super.requestHandler(clientRequest);
+  }
+}
+const requests = () => {
+  // Se tiene una petición
+  const request = new ClientAtention(RequestType.Answer, 'This is a Asnwer');
+
+  // Se inicializan clases
+  const complaint = new ComplaintRequest();
+  const answer = new AnswerRequest();
+  const technicalAssistance = new TechnicalAssistanceRequest();
+
+  // Se crean los punteros
+  complaint.setNextRequest(answer);
+  answer.setNextRequest(technicalAssistance);
+  technicalAssistance.setNextRequest(complaint);
+
+  // Cadena de responsabilidad hecha
+  console.log(complaint.requestHandler(request));
+  console.log(answer.requestHandler(request));
+  console.log(technicalAssistance.requestHandler(request));
+};
+requests();
